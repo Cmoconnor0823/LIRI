0f13cf847f44c575574fdc5b850e01f75549fc4b
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-07-10 10:26:02 -0700

    finished switch case added require fs

--- liri.js
+++ liri.js
@@ -6,18 +6,42 @@ var keys = require("./keys.js");
 // var spotify = new Spotify(keys.spotify);
 var spotify = new Spotify(keys.spotify);
 var Spotify = require('node-spotify-api');
+//require file system npm
+var fs = require("fs");
 
+
+//found a npm for styling to make it more readable
+var chalk = require('chalk');
 //grab both which serch the user wants and what they are searching for
 var searchOpt = process.argv[2];
 var searchParam = process.argv[3];
 
 //call the function to show search results
-
+userSearch(searchOpt, searchParam);
 //create a function to show the results maybe **switch case?
+function userSearch (searchOpt, searchParam) {
+    switch (searchOpt) {
+        case "spotify-this-song":
+            showSongInfo(searchParam);
+            break;
+            
+            //stop console log test and creat a default with a error -- this should help varify user input
+        case "concert-this":
+            showConcertInfo(searchParam);
+            break;
+        case "movie-this":
+            showMovieInfo(searchParam);
+            break;
+        case "do-what-it-says":
+            whatItSaysInfo(searchParam)
+            break;
 
-//stop console log test and creat a default with a error -- this should help varify user input
+        default:
+            console.log("Invalid Search. Please Input from the following options:\nspotify-this-song")
+    }
+}
 
-//create the function for searching the .text file info for a test
+//create the function for searching the "random".text file info for a test
 
 //create the 3 different search functions
 
