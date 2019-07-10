1fac835f890da373adc10de7d67cdeb7fa10693e
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-07-10 12:40:18 -0700

    cleared log.text of test data, I believe spotify is finished, but I'm having firewal problems

--- liri.js
+++ liri.js
@@ -6,8 +6,8 @@ var request = require("request");
 var keys = require("./keys.js");
 //I should now be able to access the key information by using
 // var spotify = new Spotify(keys.spotify);
-//var spotify = new Spotify(keys.spotify);
 var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 //require file system npm
 var fs = require("fs");
 
@@ -21,13 +21,13 @@ var searchParam = process.argv[3];
 //call the function to show search results
 userSearch(searchOpt, searchParam);
 //create a function to show the results maybe **switch case?
-function userSearch (searchOpt, searchParam) {
+function userSearch(searchOpt, searchParam) {
     switch (searchOpt) {
         case "spotify-this-song":
             showSongInfo(searchParam);
             break;
-            
-            //stop console log test and creat a default with a error -- this should help varify user input
+
+        //stop console log test and creat a default with a error -- this should help varify user input
         case "concert-this":
             showConcertInfo(searchParam);
             break;
@@ -50,36 +50,73 @@ function userSearch (searchOpt, searchParam) {
 //Function for spotify
 //show artist, song name, preview link, and album, o song provided default to "the sigh"by ace of base
 //use the spotify npm
+function showSongInfo(searchParam) {
+    if (searchParam === undefined) {
+        searchSong = "Ace of Base The Sign"; //default choice
+    } else {
+        searchSong = searchParam;
+    }
+    console.log("in spotify search");
+}
+spotify.search({
+    type: "track",
+    query: searchParam
+},
+    function (error, data) {
+        if (error) {
+            console.log("Error recorded: " + error);
+            return;
+        } else {
+            var songs = data.tracks.items;
 
+            for (var i = 0; i < songs.legnth; i++) {
+                console.log(chalk.blue("log.txt", "****Song Info*****\n"));
+                fs.appendFileSync("log.txt", "*****Song Info*****\n");
+                console.log(i);
+                fs.appendFileSync("log.txt", i + "\n");
+                console.log("Song name: " + songs[i].name);
+                fs.appendFileSync("log.txt", "song name: " + songs[i].name + "\n");
+                console.log("Preview song: " + songs[i].preview_url);
+                fs.appendFileSync("log.txt", "preview song: " + songs[i].preview_url + "\n");
+                console.log("Album: " + songs[i].album.name);
+                fs.appendFileSync("log.txt", "album: " + songs[i].album.name + "\n");
+                console.log("Artist(s): " + songs[i].artists[0].name);
+                fs.appendFileSync("log.txt", "artist(s): " + songs[i].artists[0].name + "\n");
+                console.log("*****************************");
+                fs.appendFileSync("log.txt", "*****************************\n");
+            }
+        }
+    });
 //function for bands in town name of the venue, venue location, date of the event using moment .js
 // use this link "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
 
-function showConcertInfo(searchParam){
-    var queryUrl ="https://rest.bandsintown.com/artists/" + searchParam + "/events?app_id=codingbootcamp";
+function showConcertInfo(searchParam) {
+    var queryUrl = "https://rest.bandsintown.com/artists/" + searchParam + "/events?app_id=codingbootcamp";
 
-    request(queryUrl, function(error, response, body) {
+    request(queryUrl, function (error, response, body) {
         // If the request is successful
         if (!error && response.statusCode === 200) {
             var concerts = JSON.parse(body);
-            for (var i = 0; i < concerts.length; i++) {  
-                console.log("**********EVENT INFO*********");  
+            for (var i = 0; i < concerts.length; i++) {
+                console.log(chalk.blue("**********EVENT INFO*********"));
                 //Append in log.txt file
                 fs.appendFileSync("log.txt", "**********EVENT INFO*********\n");
                 console.log(i);
-                fs.appendFileSync("log.txt", i+"\n");
-                console.log("Name of the Venue: " + concerts[i].venue.name);
-                fs.appendFileSync("log.txt", "Name of the Venue: " + concerts[i].venue.name+"\n");
-                console.log("Venue Location: " +  concerts[i].venue.city);
-                fs.appendFileSync("log.txt", "Venue Location: " +  concerts[i].venue.city+"\n");
-                console.log("Date of the Event: " +  concerts[i].datetime);
-                fs.appendFileSync("log.txt", "Date of the Event: " +  concerts[i].datetime+"\n");
-                console.log("*****************************");
-                fs.appendFileSync("log.txt", "*****************************"+"\n");
+                fs.appendFileSync("log.txt", i + "\n");
+                console.log(chalk.bgMagenta("Name of the Venue: " + concerts[i].venue.name));
+                fs.appendFileSync("log.txt", "Name of the Venue: " + concerts[i].venue.name + "\n");
+                console.log(chalk.bgCyan("Venue Location: " + concerts[i].venue.city));
+                fs.appendFileSync("log.txt", "Venue Location: " + concerts[i].venue.city + "\n");
+                console.log(chalk.bgGreen("Date of the Event: " + concerts[i].datetime));
+                fs.appendFileSync("log.txt", "Date of the Event: " + concerts[i].datetime + "\n");
+                console.log(chalk.blue("*****************************"));
+                fs.appendFileSync("log.txt", "*****************************" + "\n");
             }
-        } else{
-          console.log('Error occurred.');
+        } else {
+            console.log('Error occurred.');
         }
-    });}
+    });
+}
 
 
 // function for movies "omdb"
