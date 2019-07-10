ee5d0321a5b1cc79178dd30740c9867cde3091d2
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-07-10 10:55:14 -0700

    working bands in town request

--- liri.js
+++ liri.js
@@ -1,10 +1,12 @@
 //The code below is first to set the environment 
 //then next to require the api key file
 require("dotenv").config();
+
+var request = require("request");
 var keys = require("./keys.js");
 //I should now be able to access the key information by using
 // var spotify = new Spotify(keys.spotify);
-var spotify = new Spotify(keys.spotify);
+//var spotify = new Spotify(keys.spotify);
 var Spotify = require('node-spotify-api');
 //require file system npm
 var fs = require("fs");
@@ -46,7 +48,38 @@ function userSearch (searchOpt, searchParam) {
 //create the 3 different search functions
 
 //Function for spotify
+//show artist, song name, preview link, and album, o song provided default to "the sigh"by ace of base
+//use the spotify npm
 
-//function for bands in town
+//function for bands in town name of the venue, venue location, date of the event using moment .js
+// use this link "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+function showConcertInfo(searchParam){
+    var queryUrl ="https://rest.bandsintown.com/artists/" + searchParam + "/events?app_id=codingbootcamp";
+
+    request(queryUrl, function(error, response, body) {
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+            var concerts = JSON.parse(body);
+            for (var i = 0; i < concerts.length; i++) {  
+                console.log("**********EVENT INFO*********");  
+                //Append in log.txt file
+                fs.appendFileSync("log.txt", "**********EVENT INFO*********\n");
+                console.log(i);
+                fs.appendFileSync("log.txt", i+"\n");
+                console.log("Name of the Venue: " + concerts[i].venue.name);
+                fs.appendFileSync("log.txt", "Name of the Venue: " + concerts[i].venue.name+"\n");
+                console.log("Venue Location: " +  concerts[i].venue.city);
+                fs.appendFileSync("log.txt", "Venue Location: " +  concerts[i].venue.city+"\n");
+                console.log("Date of the Event: " +  concerts[i].datetime);
+                fs.appendFileSync("log.txt", "Date of the Event: " +  concerts[i].datetime+"\n");
+                console.log("*****************************");
+                fs.appendFileSync("log.txt", "*****************************"+"\n");
+            }
+        } else{
+          console.log('Error occurred.');
+        }
+    });}
+
 
 // function for movies "omdb"
